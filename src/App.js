b04577de2568cd538b35b81a3195afd9d115f014
stephje <s.jenkins3018@gmail.com>
2021-11-28 15:03:35 +0800

    Added basic components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <Header/>
+      <Navigation/>
+      <Project/>
+      <Footer/>
     </div>
   );
 }

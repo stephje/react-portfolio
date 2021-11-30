16dfcffbe8be5205359376b1618444396e345567
stephje <s.jenkins3018@gmail.com>
2021-11-30 09:30:00 +0800

    Separated content into About and Projects tabs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
-import Project from './components/Project';
 import Footer from './components/Footer';
 import React from 'react';
 
@@ -10,7 +9,6 @@ export default function App() {
       <React.Fragment>
         <Header/>
         <Navigation/>
-        <Project/>
         <Footer/>
       </React.Fragment>
   );
